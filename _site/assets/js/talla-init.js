b6761252d82d970ecb289624facea8db7fdf1d1c
Robert Crooks <rcrooks@brightcove.com>
2020-04-05 15:44:41 -0400

    talla

---
+++ _site/assets/js/talla-init.js
@@ -0,0 +1,25 @@
+var appID = "00c26be3-4421-41e6-bb00-1dee68740f67"; 
+// token for anonymous profile
+var token =
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODI4MDQzNDF9.RYf2x65FXvdDURzM h00i76KfUrYlsDLsw6mVuEv6zmE';
+// -----
+  Talla.config = Talla.init(
+    appID,
+    Talla.OptionJWTCredentials(token), 
+    Talla.OptionBotName("Video Cloud Studio Doc Search"),
+    Talla.OptionAnonymous(true),
+    Talla.OptionThemeColor('rgb(239, 108, 42)'),
+    Talla.OptionDisplayName('Search Video Cloud Studio Docs'),
+    Talla.OptionKnowledgeGroups(['e6d88869-83cb-4244-a743-49edda5d277f']),
+    Talla.OptionWideBeacon(true),
+    Talla.OptionBeaconCTA("Search Video Cloud Studio Docs"),
+    Talla.OptionOpenIcon('https://learning-services-media.brightcove.com/doc-assets/site-assets/images/site/search.png'),
+    Talla.OptionBotIcon('https://learning-services-media.brightcove.com/doc-assets/site-assets/images/site/search.png'),
+    Talla.OptionAutodetectSession
+  );
+// configure to load up as custom placement
+Talla.chat = Talla.config.configure(Talla.OptionLoadChat);
+// function to load up the chat
+ function displayChat() {
+   Talla.chat.managedDisplay(Talla.OptionDetectParent(() => { return document.querySelector('#talla_chat')} ));
+ }
