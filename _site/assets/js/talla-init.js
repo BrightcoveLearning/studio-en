ce947bd57ace2541c75b4dbd7c4dc3dccbe15d25
Bob Bailey <bbailey@brightcove.com>
2020-04-08 15:59:02 -0400

    updates

--- _site/assets/js/talla-init.js
+++ _site/assets/js/talla-init.js
@@ -1,7 +1,7 @@
-var appID = "00c26be3-4421-41e6-bb00-1dee68740f67"; 
+var appID = ""; 
 // token for anonymous profile
 var token =
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODI4MDQzNDF9.RYf2x65FXvdDURzM h00i76KfUrYlsDLsw6mVuEv6zmE';
+'';
 // -----
   Talla.config = Talla.init(
     appID,
@@ -10,7 +10,7 @@ var token =
     Talla.OptionAnonymous(true),
     Talla.OptionThemeColor('rgb(239, 108, 42)'),
     Talla.OptionDisplayName('Search Video Cloud Studio Docs'),
-    Talla.OptionKnowledgeGroups(['a4dd2ad6-2027-4428-9b38-6b2719a69e91']),
+    Talla.OptionKnowledgeGroups(['']),
     Talla.OptionWideBeacon(true),
     Talla.OptionBeaconCTA("Search Video Cloud Studio Docs"),
     Talla.OptionOpenIcon('https://learning-services-media.brightcove.com/doc-assets/site-assets/images/site/search.png'),
