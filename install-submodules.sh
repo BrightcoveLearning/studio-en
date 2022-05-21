d4a5406e822b92d96b0ce6eecbc9f0b6167af14f
Robert Crooks <rcrooks@brightcove.com>
2022-05-21 10:56:00 -0400

    adding jekyll includes and support submodules, updated nav

--- install-submodules.sh
+++ install-submodules.sh
@@ -1,5 +1,6 @@
 git submodule add https://github.com/BrightcoveLearning/s-scripts
 cd _includes
+git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-admin
 git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
 git submodule add https://github.com/BrightcoveLearning/s-social
@@ -8,6 +9,7 @@ git submodule add https://github.com/BrightcoveLearning/s-publish
 git submodule add https://github.com/BrightcoveLearning/s-media
 git submodule add https://github.com/BrightcoveLearning/s-analytics
 git submodule add https://github.com/BrightcoveLearning/s-general
+git submodule add https://github.com/BrightcoveLearning/s-support
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-admin-img
@@ -18,5 +20,6 @@ git submodule add https://github.com/BrightcoveLearning/s-publish-img
 git submodule add https://github.com/BrightcoveLearning/s-media-img
 git submodule add https://github.com/BrightcoveLearning/s-analytics-img
 git submodule add https://github.com/BrightcoveLearning/s-general-img
+git submodule add https://github.com/BrightcoveLearning/s-support-img
 cd ..
 git submodule update --init --recursive
