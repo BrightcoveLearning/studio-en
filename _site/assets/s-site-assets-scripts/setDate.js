22506f7923d5432ec457c5dd59c25d8b64dda12b
Bob Bailey <bbailey@brightcove.com>
2022-05-31 09:05:56 -0400

    updates

--- _site/assets/s-site-assets-scripts/setDate.js
+++
@@ -1,8 +0,0 @@
-var BCLS_set_date = ( function (window, document) {
-  var  bc_copyright = document.getElementById('bc_copyright'),
-    y = new Date().getFullYear();
-  if (bc_copyright) {
-    var c = bc_copyright.textContent;
-    bc_copyright.innerHTML = '&copy;' + y + '&nbsp;' + c;
-  }
-})(window, document);
